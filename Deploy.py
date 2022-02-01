4fc4d2ac23f2e5600c1aff4767f5cb845ae5c298
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2022-02-02 00:17:10 +0530

    Completed base structure for deploy and hand_tracking

--- Deploy.py
+++ Deploy.py
@@ -1,11 +1,13 @@
 import cv2
 import mediapipe as mp
 import time
+from Hand_Tracking import Coordinates
 
 cap = cv2.VideoCapture(0)
 
 while True:
     success, frame = cap.read()
+    frame = Coordinates(frame)
     
     cv2.imshow('Feed', frame)
     if cv2.waitKey(20) & 0xFF == ord('x'):
