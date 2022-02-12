fe68e5f325da36f2707026e03b7925759bee1e8d
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2022-02-12 18:41:23 +0530

    base model ready, deploy completed

--- Deploy.py
+++ Deploy.py
@@ -1,10 +1,15 @@
 import cv2
+import numpy as np
 import HandTracking_GestureRecognition_Module as hgm
 
 def main():
     cap = cv2.VideoCapture(0)
     cap.set(3, 1280)
     cap.set(4, 720)
+
+    canvas = np.zeros((720, 1280, 3), dtype = 'uint8')
+
+    xp, yp = 0, 0
 
     detector = hgm.HandDetector()
 
@@ -17,7 +22,30 @@ def main():
 
         if len(lm_list):
             fingers = detector.FindGesture()
-            print(fingers)
+            xi, yi = lm_list[8][1:]
+
+            if fingers[0] == 1 and fingers[1] == 0 and fingers[2] == 0 and fingers[3] == 0:
+                cv2.circle(frame, (xi, yi), 20, (0, 0, 255), -1)
+                if xp == 0 and yp == 0:
+                    xp, yp = xi, yi
+                
+                cv2.line(canvas, (xp, yp), (xi, yi), (0, 0, 255), 20)
+                xp, yp = xi, yi
+            
+            if fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0:
+                xp, yp = 0, 0
+
+            if fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1:
+                cv2.circle(frame, (xi, yi), 60, (0, 0, 0), -1)
+                cv2.circle(canvas, (xi, yi), 60, (0, 0, 0), -1)
+                xp, yp = 0, 0
+
+        gray = cv2.cvtColor(canvas, cv2.COLOR_BGR2GRAY)
+        _, ImgInv = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY_INV)
+        ImgInv = cv2.cvtColor(ImgInv, cv2.COLOR_GRAY2BGR)
+        frame = cv2.bitwise_and(frame, ImgInv)
+        frame = cv2.bitwise_or(frame, canvas)
+
     
         cv2.imshow('Live', frame)
 
